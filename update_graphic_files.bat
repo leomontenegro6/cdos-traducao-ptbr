3675529c6d533ac8ea2d19d7e67411a657d3cb61
Leonardo Montenegro <leomontenegro6@gmail.com>
2025-01-02 12:58:01 -0300

    Editada fonte de diálogos média, pra evitar caracteres colados.

--- update_graphic_files.bat
+++ update_graphic_files.bat
@@ -12,6 +12,7 @@
 setlocal enabledelayedexpansion
 set bc_files=data\bc\f_abil.dat data\bc\f_conf.dat data\bc\f_ene0.dat data\bc\f_itemu.dat data\bc\f_libr.dat data\bc\f_mgs.dat data\bc\f_pal0.dat data\bc\f_sd_e.dat
 set sc_files=data\sc\f_area_e00.dat data\sc\f_area_e01.dat data\sc\f_area_e02.dat data\sc\f_area_e03.dat data\sc\f_area_e04.dat data\sc\f_area_e05.dat data\sc\f_com01.dat data\sc\f_com05.dat data\sc\f_eset.dat data\sc\f_gover3.dat data\sc\f_hai.dat data\sc\f_lice.dat data\sc\f_mainob.dat data\sc\f_mark_e.dat data\sc\f_mix_e.dat data\sc\f_name0.dat data\sc\f_net.dat data\sc\f_res_e0.dat data\sc\f_retry.dat data\sc\f_scrap.dat data\sc\f_shop.dat data\sc\f_tiobj1.dat data\sc\f_tit_e2.dat data\sc\f_top0.dat data\sc\f_top1.dat data\sc\f_trade.dat
+set font_files=data\font\LD_font_u12.DAT
 
 :: Copiando os arquivos
 echo Copiando arquivos da pasta 'bc'
@@ -21,6 +22,10 @@ for %%f in (%bc_files%) do (
 echo Copiando arquivos da pasta 'sc'
 for %%f in (%sc_files%) do (
     copy ".\Graficos\Editados\%%f" ".\Extracted files Castlevania - Alvor da Tristeza (BR)\sc\"
+)
+echo Copiando arquivos da pasta 'font'
+for %%f in (%font_files%) do (
+    copy ".\Graficos\Editados\%%f" ".\Extracted files Castlevania - Alvor da Tristeza (BR)\font\"
 )
 
 echo Copiados arquivos de graficos com sucesso
